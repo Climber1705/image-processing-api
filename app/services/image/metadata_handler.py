3bb6b5b638b8e3cf0f342bbc9401910b3f6660f0
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-05 09:43:40 +0100

    Update metadata_handler.py

--- app/services/image/metadata_handler.py
+++ app/services/image/metadata_handler.py
@@ -46,7 +46,3 @@ class ImageMetadataExtractor:
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail=f"Failed to get image info: {str(e)}",
             )
-
-
-def get_image_metadata_extractor() -> ImageMetadataExtractor:
-    return ImageMetadataExtractor()
