88b20ba79331b21795b69f09ebac9daf40caf9f7
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-05 09:42:09 +0100

    Add storage.py

---
+++ app/dependencies/storage.py
@@ -0,0 +1,18 @@
+from fastapi import Depends
+
+from app.dependencies.utils import get_directory_manager, get_simple_image_validator, get_file_path_resolver
+from app.services.image.storage.local_storage import LocalImageStorage
+from app.utils.file_operations.directory_utils import DirectoryManager
+from app.utils.validator.simple_validator import SimpleImageValidator
+from app.utils.file_operations.file_utils import FilePathResolver
+
+def get_local_image_storage(
+    directory_manager: DirectoryManager = Depends(get_directory_manager),
+    image_validator: SimpleImageValidator = Depends(get_simple_image_validator),
+    file_resolver: FilePathResolver = Depends(get_file_path_resolver),
+) -> LocalImageStorage:
+    return LocalImageStorage(
+        directory_manager=directory_manager,
+        image_validator=image_validator,
+        file_resolver=file_resolver,
+    )
