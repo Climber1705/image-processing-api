5f0ad79931595e1d5e0cf357344a671b6dd412ef
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-05 08:57:35 +0100

    Update detection_responses.py

--- app/schemas/detection/detection_responses.py
+++ app/schemas/detection/detection_responses.py
@@ -12,8 +12,12 @@ class BoundingBoxResponse(BaseModel):
     message: str
     image_path: str
     detections: List[DetectionBox]
+    model_name: str
+    model_version: str | None = None
 
 
 class DetectedObjectsResponse(BaseModel):
     message: str
     detected_objects: List[DetectionBox]
+    model_name: str
+    model_version: str | None = None
