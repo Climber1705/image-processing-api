1e299e9d1d086b3854381e855f5d1e3408d4c241
Thomas Trotter <thomas.trotter2006@gmail.com>
2026-07-05 18:23:42 +0100

    Update repositories.py

--- app/dependencies/repositories.py
+++ app/dependencies/repositories.py
@@ -1,9 +1,10 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
-
 from app.db.session import get_db
 from app.media.repository import ImageRepository
 
-def get_image_repository(session: Session = Depends(get_db)) -> ImageRepository:
+def get_image_repository(
+    session: Session = Depends(get_db)
+) -> ImageRepository:
     return ImageRepository(session=session)
